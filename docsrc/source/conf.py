4d00cf458179b48bebfc2ff63533aa3f937d00ab
Romain <the.spartan.proj@gmail.com>
2022-09-07 10:55:46 -0400

    icons

--- docsrc/source/conf.py
+++ docsrc/source/conf.py
@@ -89,7 +89,7 @@ todo_include_todos = True
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
 html_logo = "../logo/logo.png"
-
+html_favicon = '../logo/favicon.png'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
